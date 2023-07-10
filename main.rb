d1783a3999b48bf0f09573726d643edebe0a5f0d
jlvFlores <jluisvazquez.jv@gmail.com>
2023-07-10 10:55:20 -0600

    Fix linter errors

--- main.rb
+++ main.rb
@@ -3,65 +3,51 @@ class Main
     interface
   end
 
+  OPTIONS = {
+    1 => 'future method 1',
+    2 => 'future method 2',
+    3 => 'future method 3',
+    4 => 'future method 4',
+    5 => 'future method 5',
+    6 => 'future method 6',
+    7 => 'future method 7',
+    8 => 'future method 8',
+    9 => 'future method 9',
+    10 => 'future method 10',
+    11 => 'future method 11',
+    12 => 'future method 12',
+    13 => 'Exit'
+  }.freeze
+
   def interface
     show_console_options
     option = get_user_input('Enter your choice: ').to_i
     select_option(option)
-    sleep(1)
-    if option == 13
-      exit
-    else 
-      interface
-    end
+    interface
   end
 
   def show_console_options
-    options_array = ['List all books', 'List all music albums', 'List all movies', 'List all games', 'List all genres',
-                     'List all labels', 'List all authors', 'List all sources', 'Add book', 'Add a music album', 'Add movie',
-                     'Add a game', 'Exit']
     puts "\nWelcome to the Ruby Console App!"
     puts "\nPlease choose an option from the list below:"
     puts '-------------------------------------------'
+    options_array = ['List all books', 'List all music albums', 'List all movies', 'List all games', 'List all genres',
+                     'List all labels', 'List all authors', 'List all sources', 'Add book', 'Add a music album',
+                     'Add movie', 'Add a game', 'Exit']
     options_array.each_with_index do |option, index|
       puts "#{index + 1}. #{option}"
     end
   end
 
   def select_option(option)
-    case option
-    when 1
-      puts 'future method 1'
-    when 2
-      puts 'future method 2'
-    when 3
-      puts 'future method 3'
-    when 4
-      puts 'future method 4'
-    when 5
-      puts 'future method 5'
-    when 6
-      puts 'future method 6'
-    when 7
-      puts 'future method 7'
-    when 8
-      puts 'future method 8'
-    when 9
-      puts 'future method 9'
-    when 10
-      puts 'future method 10'
-    when 11
-      puts 'future method 11'
-    when 12
-      puts 'future method 12'
-    when 13
-      puts 'Exit'
+    if OPTIONS.key?(option)
+      puts OPTIONS[option]
+      exit if option == 13 # Exit if option is 13
     else
       puts '-------------------------------------------'
       puts "\nInvalid option, try again!"
       puts "\n-------------------------------------------"
-      sleep(1)
-      Main.new
     end
+    sleep(1)
   end
 
   def get_user_input(prompt)
