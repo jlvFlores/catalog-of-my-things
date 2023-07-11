049cdfd0d8a17f395a1d1dedc6db8eb8444248d2
jlvFlores <jluisvazquez.jv@gmail.com>
2023-07-11 07:40:37 -0600

    Refactor spec test

--- spec/music_album_spec.rb
+++ spec/music_album_spec.rb
@@ -1,9 +1,11 @@
 require './genre'
 require './music_album'
+require 'date'
 
 describe MusicAlbum do
+  let(:genre) { Genre.new('Rock') }
+
   context 'when receiving valid parameters' do
-    let(:genre) { Genre.new('Rock') }
     let(:music_album) { MusicAlbum.new(genre, '2012-07-10', true) }
 
     it 'returns a valid MusicAlbum object' do
@@ -12,16 +14,16 @@ describe MusicAlbum do
   end
 
   context 'checking can_be_archived? method' do
-    let(:genre) { Genre.new('Rock') }
+    let(:today) { Date.today.to_s }
 
     it 'when date is LESS than 10 years and is NOT on spotify' do
-      music_album = MusicAlbum.new(genre, '2023-07-10', false)
+      music_album = MusicAlbum.new(genre, today, false)
       music_album.move_to_archive
       expect(music_album.archived).to eq(false)
     end
 
     it 'when date is LESS than 10 years and is on spotify' do
-      music_album = MusicAlbum.new(genre, '2023-07-10', true)
+      music_album = MusicAlbum.new(genre, today, true)
       music_album.move_to_archive
       expect(music_album.archived).to eq(false)
     end
