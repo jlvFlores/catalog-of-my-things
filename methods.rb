639b9105f4e4463380ea31de07dc9b0bdaa8ccc3
jlvFlores <jluisvazquez.jv@gmail.com>
2023-07-11 10:11:43 -0600

    Update main.rb and Add methods module

---
+++ methods.rb
@@ -0,0 +1,42 @@
+module Methods
+  def list_books
+    puts 'We are going to list the books here'
+  end
+
+  def list_music_albums
+    puts 'We are going to list the albums here'
+  end
+
+  def list_games
+    puts 'We are going to list the games here'
+  end
+
+  def list_genres
+    puts 'We are going to list the genres here'
+  end
+
+  def list_labels
+    puts 'We are going to list the labels here'
+  end
+
+  def list_authors
+    puts 'We are going to list the authors here'
+  end
+
+  def add_book
+    puts 'We are going to add an book here'
+  end
+
+  def add_music_album
+    puts 'We are going to add an music album here'
+  end
+
+  def add_game
+    puts 'We are going to add an game here'
+  end
+
+  def exit_app
+    puts 'Thank you for using our app'
+    exit
+  end
+end
