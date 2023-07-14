e1b1701aaa07c36b1b7c6ef9ee3ec99cacd189b6
jlvFlores <jluisvazquez.jv@gmail.com>
2023-07-14 10:33:51 -0600

    Update can_be_archived?

--- item.rb
+++ item.rb
@@ -2,11 +2,14 @@ require './label'
 require './genre'
 require './author'
 require 'date'
+
 class Item
   attr_reader :id, :publish_date, :label, :genre, :author, :archived
+
   def initialize(category, publish_date)
     @id = Random.rand(1...1000)
     @publish_date = Date.parse(publish_date)
+
     case category
     when Label
       @label = category
@@ -16,13 +19,17 @@ class Item
       @author = category
     end
     category.add_item(self)
+
     @archived = false
   end
+
   def move_to_archive
     @archived = can_be_archived?
   end
+
   private
+
   def can_be_archived?
-    Date.today.year - @publish_date.year > 10
+    (Date.today - @publish_date).to_i > (10 * 365)
   end
 end
