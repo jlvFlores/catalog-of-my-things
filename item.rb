602ddab15f21e8a8cbe7c74c6d500fa18018010d
Cisco <ciscophrenzy@gmail.com>
2023-07-14 09:36:33 -0600

    Update item and label

--- item.rb
+++ item.rb
@@ -1,36 +1,27 @@
-require './genre'
 require './label'
+require './genre'
+require './author'
 require 'date'
-
 class Item
-  attr_reader :archived, :label, :genre, :author, :source, :id, :publish_date
-
+  attr_reader :id, :publish_date, :label, :genre, :author, :archived
   def initialize(category, publish_date)
     @id = Random.rand(1...1000)
     @publish_date = Date.parse(publish_date)
-    @label = nil
     case category
-    when Genre
-      @genre = category
-      category.add_item(self)
-    # when Author
-    #   @author = category
-
     when Label
       @label = category
+    when Genre
+      @genre = category
+    when Author
+      @author = category
     end
-
+    category.add_item(self)
     @archived = false
   end
-
   def move_to_archive
     @archived = can_be_archived?
   end
-
-  def add_label(label)
-    @label = label
-  end
-
+  private
   def can_be_archived?
     Date.today.year - @publish_date.year > 10
   end
